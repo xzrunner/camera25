09db9f1f73a726a78fdba6248be4bf149061963f
zz <zhuguang@ejoy.com>
2016-06-07 13:38:30 +0800

    [ADDED] android makefile

---
+++ Android.mk
@@ -0,0 +1,18 @@
+INNER_SAVED_LOCAL_PATH := $(LOCAL_PATH)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := camera25
+
+LOCAL_C_INCLUDES  := \
+	${LUA_SRC_PATH} \
+	${SM_SRC_PATH} \
+
+LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.c" -print)) \
+
+include $(BUILD_STATIC_LIBRARY)	
+
+LOCAL_PATH := $(INNER_SAVED_LOCAL_PATH)
