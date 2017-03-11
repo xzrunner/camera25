3838b9f35535beb6955940368c4536330e27cbf2
yangkaibo <yangkaiboo@163.com>
2017-03-11 17:21:23 +0800

    build android

--- Android.mk
+++ Android.mk
@@ -13,6 +13,9 @@ LOCAL_C_INCLUDES  := \
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.c" -print)) \
 
+LOCAL_STATIC_LIBRARIES := \
+	sm \
+
 include $(BUILD_STATIC_LIBRARY)	
 
 LOCAL_PATH := $(INNER_SAVED_LOCAL_PATH)
